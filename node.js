b3e8988aef681d8481608e228f74efa83a6bf6ab
zhizhizzzzzzz <162452616+zhizhizzzzzzz@users.noreply.github.com>
2024-09-16 19:52:34 +0800

    Update node.js

--- node.js
+++ node.js
@@ -8,15 +8,20 @@ const udpServer = dgram.createSocket('udp4');
 udpServer.bind(8081, '192.168.10.230');
 
 // 创建WebSocket服务器
-const wss = new WebSocket.Server({ port: 8081 });
+const wss = new WebSocket.Server({ port: 8080 });
 
 wss.on('connection', (ws) => {
   console.log('WebSocket连接已建立');
-  
-  udpServer.on('message', (message) => {
-    console.log('接收到UDP数据：' + message);
-    ws.send(message);
+});
+
+udpServer.on('message', (message) => {
+  console.log('接收到UDP数据');
+  // 向所有连接的客户端广播数据
+  wss.clients.forEach((client) => {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(message);
+    }
   });
 });
 
-console.log('服务器已启动，正在监听192.168.10.230:8000');
+console.log('服务器已启动，正在监听192.168.10.230:8081');
